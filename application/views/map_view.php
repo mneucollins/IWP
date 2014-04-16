9085e0c3d72c8ac78c0b9fd1999713383f68d25c
mneucollins <mneucollins@gmail.com>
2014-04-15 19:38:36 -0500

    mapview now set to load annual map. Database password reset

--- application/views/map_view.php
+++ application/views/map_view.php
@@ -40,58 +40,69 @@
         </div>
     </div>
 
-<script>
-var map = L.map('map')
-
-var mywms = L.tileLayer.wms("http://dsph-dev.provost.uiowa.edu:8080/geoserver/opengeo/wms?service=WMS", {
-    layers: 'opengeo:cshapes_042_mollweide',
-    format: 'image/png',
-    transparent: true,
-    version: '1.1.0',
-    attribution: "<a href='http://nils.weidmann.ws/projects/cshapes'>cshapes country boundaries</a>"
-});
-mywms.addTo(map);
-
-function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-        alert(feature.properties.popupWidth);
-    }
-}
-
-//{minWidth:feature.properties,popupWidth;maxWidth:feature.properties.popupWidth;}
-//returned from PostGIS query "{"type":"Point","coordinates":[54.8885812055762,35.4287280157253]}"
-
-
-var markers = <?php echo $geojson; ?>;
-
-
-L.geoJson(markers, {
-	style: function (feature) {
-		return feature.properties && feature.properties.style;
-	},
-	onEachFeature: onEachFeature,
-	pointToLayer: function (feature, latlng) {
-		return L.circleMarker(latlng, {
-			radius: feature.radius,
-			fillColor: "#ff7800",
-			color: "#000",
-			weight: 1,
-			opacity: 1,
-			fillOpacity: 0.8
+<?php if ($residency < 1978) : ?>
+	<script>
+		var map = L.map('map')
+		var base = L.tileLayer.wms("http://dsph-dev.provost.uiowa.edu:8080/geoserver/IWP/wms?service=WMS", {
+		    layers: 'IWP:cshapes_mollweide_042_LandMass',
+		    format: 'image/png',
+		    transparent: true,
+		    version: '1.1.0',
+		    attribution: "<a href='http://nils.weidmann.ws/projects/cshapes'>cshapes country boundaries</a>"
 		});
+		base.addTo(map);
+	</script>
+<?php endif; ?>
+
+
+<script>
+	var mywms = L.tileLayer.wms("http://dsph-dev.provost.uiowa.edu:8080/geoserver/IWP/wms?service=WMS", {
+	    layers: '<?php echo "IWP:cshapes_mollweide_042_$residency"; ?>' ,
+	    format: 'image/png',
+	    transparent: true,
+	    version: '1.1.0',
+	    attribution: "<a href='http://nils.weidmann.ws/projects/cshapes'>cshapes country boundaries</a>"
+	});
+	mywms.addTo(map);
+	
+	
+	function onEachFeature(feature, layer) {
+	    // does this feature have a property named popupContent?
+	    if (feature.properties && feature.properties.popupContent) {
+	        layer.bindPopup(feature.properties.popupContent);
+	    }
 	}
-}).addTo(map);
-map.setView([15,10], 2.2);
-
-/*
-map.on('click', function(e) { alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng) })
-*/
-
-
-
+	
+	//{minWidth:feature.properties,popupWidth;maxWidth:feature.properties.popupWidth;}
+	//returned from PostGIS query "{"type":"Point","coordinates":[54.8885812055762,35.4287280157253]}"
+	
+	
+	var markers = <?php echo $geojson; ?>;
+	
+	
+	L.geoJson(markers, {
+		style: function (feature) {
+			return feature.properties && feature.properties.style;
+		},
+		onEachFeature: onEachFeature,
+		pointToLayer: function (feature, latlng) {
+			return L.circleMarker(latlng, {
+				radius: feature.radius,
+				fillColor: "#ff7800",
+				color: "#000",
+				weight: 1,
+				opacity: 1,
+				fillOpacity: 0.8
+			});
+		}
+	}).addTo(map);
+	map.setView([15,10], 2.2);
+	
+	/*
+	map.on('click', function(e) { alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng) })
+	*/
 </script>
+
 </body>
 </html>
 
